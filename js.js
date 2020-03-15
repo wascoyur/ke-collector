9ad0c69b0d7ecef680131e4ef1674c1c36eab78e
wascoyur <mytem20007@gmail.com>
2020-03-15 17:06:06 +0500

    Добавлен слушатель инпута

--- js.js
+++ js.js
@@ -1,2 +1,10 @@
-let forms = document.forms;
-console.log(forms);
+let elements = document.getElementsByTagName('input');
+for (const iterator of elements) {
+console.log(iterator)
+}
+const listener = this.addEventListener('input', (event)=>{
+    if(event.target && event.type == 'input'){
+        console.log('сработал инпут');
+        
+    }
+});
