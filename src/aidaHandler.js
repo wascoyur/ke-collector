2ee918b7374c0a3ed9eab7e3ba04f0dc688c9f68
wascoyur <mytem20007@gmail.com>
2020-05-31 11:29:32 +0500

    не удалось вынести слушатель в другой файл

--- src/aidaHandler.js
+++ src/aidaHandler.js
@@ -1,7 +1,27 @@
-export default class AidaHandler{
-	static getXmlFile(fileName) {
-		let dataFile = fileName[0];
-		return dataFile;
-	};
+let convert = require('xml-js');
+export default class AidaHandler {
+	constructor(inputForm) {
+		this.form = document.getElementById(inputForm);
+		this.xmlContent = '';
+	}
+	
+	getFile() {
+		this.form.addEventListener('change', (event) => {
+			let file = event.currentTarget.files[0];
+			let reader = new FileReader();
+			reader.onload = function (e) {
+				this.xmlContent = e.target.result
+			}
+			reader.readAsText(file)
+		})
+	}
+ 
+	convertFile(input) {
+	  let options = {compact: true, ignoreComment: true, spaces: 4};
+	  let jsObject = convert.xml2json(input, options);
+	  return jsObject;
+	}
+		// console.log(inputFile);
 	 
+			
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import './css/style.css';
-let convert = require('xml-js');
+
 import AidaHandler from './aidaHandler';
 
-let dataFromAida = new AidaHandler();
-let inputForm = document.getElementById('file');
-let inputFile = '';
-inputForm.addEventListener('change', (event) => {
-  let file = event.currentTarget.files[0];
-  let reader = new FileReader();
-  reader.onload = function(e){
-    inputFile = getFile(e.target.result)
-  }
-  reader.readAsText(file)
-  
-} )
-function getFile(input) {
-  let file = input;
-  let options = {compact: true, ignoreComment: true, spaces: 4};
-  let jsObject = convert.xml2json(input, options);
-  return jsObject;
-}
-console.log(inputFile);
+let dataFromAida = new AidaHandler('file');
+dataFromAida.getFile();
+dataFromAida.convertFile;
+console.log(dataFromAida.xmlContent);
+
 
 
 
