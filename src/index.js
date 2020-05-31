f9f0771503c732f6e533a7ff00c0e1b704b59b59
wascoyur <mytem20007@gmail.com>
2020-05-31 17:19:01 +0500

    данные из .xml читаются, парсятся

--- src/index.js
+++ src/index.js
@@ -12,13 +12,16 @@ formInputAidaFile.addEventListener('change', (ev) => {
 				.then((xml) => {
 					let parser = new DOMParser();
 					let xmlDOM = parser.parseFromString(xml, 'application/xml');
-					let pages = xmlDOM.querySelectorAll('Page');
-					pages.forEach(pageXmlNode => {
-						console.log(pageXmlNode.textContent);
-						if (pageXmlNode) {
-							
-						}
-					})
+					let pages = xmlDOM.querySelectorAll('Item');
+					parserData(pages);
 			})
 		})
 })
+function parserData(nodes) {
+	nodes.forEach(pageXmlNode => {
+		let arChildrNodes = Array.prototype.slice.call(pageXmlNode.childNodes);
+		let title = arChildrNodes[0].textContent;
+		let value = pageXmlNode.childNodes[arChildrNodes.length - 1].textContent;
+		configurationUnit[title] = value;
+	})
+}
