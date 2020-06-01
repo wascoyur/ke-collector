bbc93e57f964d80cf8a64fdb241983d505399222
wascoyur <mytem20007@gmail.com>
2020-06-01 21:13:17 +0500

    файлы читаются

--- src/aidaHandler.js
+++ src/aidaHandler.js
@@ -5,18 +5,20 @@ export default class AidaHandler {
 	}
 
 	getFile(event) {
-			let file = event.target.value;
+			let file = event.target.files[0];
 			let reader = new FileReader();
 
 			reader.onload = function (e) {
-				console.log(e.reader.result);
+				// this.xmlContent = reader.result;
 				return reader.result;
 			};
 			reader.onerror = function () {
 				console.log(reader.error);
 			};
 			reader.readAsText(file)
-
+	}
+	uploadFile() {
+		
 	}
 
 	convertFile(input) {
