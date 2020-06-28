d564e8134af844eb60c623b2df52ec4bf782ad73
wascoyur <mytem20007@gmail.com>
2020-06-28 16:10:59 +0500

    реализую обход xmlPages

--- src/js/index.js
+++ src/js/index.js
@@ -3,45 +3,30 @@ import ('./xml2json.js');
 let formInputAidaFile = document.getElementById('aida-xml');
 let configurationUnit = {};
 let myTable = document.querySelector('#aida-out');
-let res = 'one';
-const arrStdTemplReport = ['Имя ЦП CPUID', 'Тип памяти', 'Всего', 'Системная плата', 'Аппаратный адрес', 'Дисковый накопитель'];
+let res = '';
+const arrStdTemplReport = ['Тип ЦП', 'Чипсет системной платы', 'Системная память', 'Всего', 'Системная плата', 'Аппаратный адрес', 'Дисковый накопитель'];
 const btnViewStndReportAida = document.getElementById('btn-stnd-set');
 const btnViewAllReportAida = document.getElementById('btn-view-all');
 
 formInputAidaFile.addEventListener('change', (ev) => {
 	ev.preventDefault;
 	getFileFromDrive(ev);
-	let parser = new DOMParser();
-	let xmlDOM = parser.parseFromString(res, 'application/xml');
-	// let pages = xmlDOM.querySelectorAll('Page');
-	let pages = xml2json(xmlDOM)
-	console.log(pages);
-	configurationUnit = JSON.parse(pages);
-	lamper();
-	// parserData(pages);
-	// viewer(configurationUnit);
 })
 
 btnViewAllReportAida.addEventListener('click', (ev) => {
-	viewer(configurationUnit)
+	parserData();
+	tableCreater(configurationUnit);
+
 })
 btnViewStndReportAida.addEventListener('click', () =>{
-	viewerStnd(configurationUnit);
+	parserData();
+	tableCreater(viewerStnd(configurationUnit)) ;
 })
 
 /*=================== functions ======================  */
 function parserData(nodes) {
-	console.dir(nodes);
-	nodes.forEach(pageXmlNode => {
-		let arChildrNodes = Array.prototype.slice.call(pageXmlNode.childNodes);
-		console.dir(arChildrNodes);
-		let title = arChildrNodes[0].textContent;
-		let value = pageXmlNode.childNodes[arChildrNodes.length - 1].textContent;
-		configurationUnit[title] = value;
-	})
-
-
-
+	let pages = X2J.parseXml(res);
+	configurationUnit = pages[0].Report[0].Page;
 }
 function getFileFromDrive(event) {
 	let file = event.target.files[0];
@@ -53,11 +38,13 @@ function getFileFromDrive(event) {
 	reader.onloadstart = function (params) {
 		console.log('begin loaded')
 	}
-	reader.onload = function (params) {
+	reader.loadend = function () {
 		console.log('Загрузка файла закончена')
+
 	}
 	reader.onload = function (e) {
 		res = reader.result
+		lamper();
 	};
 	reader.onerror = function () {
 		console.log(reader.error);
@@ -65,7 +52,7 @@ function getFileFromDrive(event) {
 	};
 
 }
-function viewer(data) {
+function tableCreater(data) {
 	const tbody = myTable.querySelector('tbody');
 	for (const key in data) {
 		let row = document.createElement('tr');
@@ -86,17 +73,21 @@ function viewer(data) {
 }
 function viewerStnd(data){
 	let arrStndReport = {};
-	arrStdTemplReport.forEach(el =>{
-		for(const key in data){
-			if (el == key) {
-				arrStndReport[data] = key;
+	data.forEach(page => {
+		console.dir(page)
+	})
+	stndDataReport.forEach(el =>{
+		arrStdTemplReport.forEach(elin => {
+			console.log(`ключ: ${el.Title[0].jValue}`, `значение: ${el.Value[0].jValue}` );
+			if (elin === el.Title[0].jValue) {
+				arrStndReport[el.Title[0].jValue] = el.Value[0].jValue;
 			}
-		}
+		})
 	})
 	return arrStndReport;
 }
 function lamper(){
-	if (Object.keys(configurationUnit).length !== 0) {
+	if (res !== 0) {
 		const lamp = document.querySelector('#lamp');
 		document.querySelector('#lamp-text').textContent = 'Есть внешние данные';
 		lamp.style.backgroundColor = 'green';
